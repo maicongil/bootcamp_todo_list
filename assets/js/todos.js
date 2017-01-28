03805fe96813562096a82d017741632a9fa74dd1
maicon <gugamaicon@gmail.com>
2017-01-28 19:32:17 -0200

    remove todo item

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,8 +1,9 @@
-
+//mark todo item as complete
 $("li").on("click", function(){
     $(this).toggleClass("completed");
 });
 
+//remove todo item
 $(".removeBtn").on("click", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
