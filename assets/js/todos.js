f5767b95d80bf15611d89a9bff64a81bf3b4db7b
maicon <gugamaicon@gmail.com>
2017-01-28 19:30:34 -0200

    mark todo item complete

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,11 @@
 
 $("li").on("click", function(){
     $(this).toggleClass("completed");
-})
+});
+
+$(".removeBtn").on("click", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
